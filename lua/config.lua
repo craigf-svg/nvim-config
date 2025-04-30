31f6a84049e0f97fd55ec68417f87526d81eb500
craigf-svg <35483367+craigf-svg@users.noreply.github.com>
2025-04-30 15:51:35 -0400

    Add javascript
    Add js debugging to config.lua, also add a space between string and value in both python and js prints

--- lua/config.lua
+++ lua/config.lua
@@ -4,6 +4,14 @@ vim.api.nvim_create_autocmd("FileType", {
 	pattern = "python",
 	callback = function()
 		print("[config.lua]: python filetype detected")
-		vim.keymap.set("n", "<leader>p", "yiwoprint('<C-r>\"', <C-r>\")<Esc>", { noremap = true, buffer = true })
+		vim.keymap.set("n", "<leader>p", "yiwoprint('<C-r>\" ', <C-r>\")<Esc>", { noremap = true, buffer = true })
+	end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "javascript",
+	callback = function()
+		print("[config.lua]: javascript filetype detected")
+		vim.keymap.set("n", "<leader>p", "yiwoconsole.log('<C-r>\" ', <C-r>\")<Esc>", { noremap = true, buffer = true })
 	end,
 })
