0a59137a3f3de92a51486b2c99cdaf93624b61ad
craigf-svg <35483367+craigf-svg@users.noreply.github.com>
2025-05-12 11:27:15 -0400

    Update config.lua
    Simplify prints

--- lua/config.lua
+++ lua/config.lua
@@ -5,7 +5,7 @@ vim.api.nvim_create_autocmd("FileType", {
 	pattern = "python",
 	callback = function()
 		print("[config.lua]: python filetype detected")
-		vim.keymap.set("n", "<leader>p", "yiwoprint('<C-r>\" ', <C-r>\")<Esc>", { noremap = true, buffer = true })
+		vim.keymap.set("n", "<leader>p", "yiwoprint('<C-r>\"', <C-r>\")<Esc>", { noremap = true, buffer = true })
 	end,
 })
 
@@ -13,6 +13,6 @@ vim.api.nvim_create_autocmd("FileType", {
 	pattern = "javascript",
 	callback = function()
 		print("[config.lua]: javascript filetype detected")
-		vim.keymap.set("n", "<leader>p", "yiwoconsole.log('<C-r>\" ', <C-r>\")<Esc>", { noremap = true, buffer = true })
+		vim.keymap.set("n", "<leader>p", "yiwoconsole.log('<C-r>\"', <C-r>\")<Esc>", { noremap = true, buffer = true })
 	end,
 })
